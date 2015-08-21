f220b92a556e5f759195b0f10f1f434955505a02
xavier canal <xaviercanal66@gmail.com>
2015-08-21 14:52:37 +0400

    Update cities.php

--- views/default/panels/cities.php
+++ views/default/panels/cities.php
@@ -1,8 +1,7 @@
 <div class="box-city box">
-	<h1><i class="fa fa-university"></i> CITIES</h1>
+	<h1><i class="fa fa-university"></i> CITIES:CITES</h1>
 	<section>
-		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		Nous vivons tous dans une cité, une commune, une ville... peu importe son nom, sa taille. Elle est la somme de nos individualités, de nos projets. Elle se dessine chaque jour et c'est un monde à partager, à faire vivre. C'est tout le propos. Donner du sens au mot communauté. 
 		<u><a href="#" class="text-white" onclick="showPanel('box-login')">Connect to your city</a></u>
 		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.
 	</section>
