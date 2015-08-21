a863450869df6ef9c3e2294dfdb4b0a1f1c7ffab
xavier canal <xaviercanal66@gmail.com>
2015-08-21 14:59:54 +0400

    Update people.php

--- views/default/panels/people.php
+++ views/default/panels/people.php
@@ -1,8 +1,8 @@
 <div class="box-people box">
 	<h1><i class="fa fa-user"></i> PEOPLE </h1>
 	<section>
-		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-		tempor incididunt ut labore et dolore magna aliqua. 
+		Communecter est pour nous tous. Parce que nous sommes uniques, parce ce que nous sommes créatifs, que nous avons quelque chose à dire, à proposer.Les hommes
+		et les femmes batissent la communauté. Tous ensemble à travers nos différences, nos origines, notre histoire nous sommes uniques et plusieurs. 
 		<u><a class="text-white" href="#" onclick="showAjaxPanel( baseUrl+'/'+moduleId+'/person/invitesv?isNotSV=1', 'INVITE SOMEONE','share-alt' )"> Invite someone </a> </u>
 		Ut enim ad minim veniam,
 		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.
