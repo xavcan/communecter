4f64d5492e4e9705a6e7ce5a71b9a3661be327b6
xavier canal <xaviercanal66@gmail.com>
2015-08-21 14:09:15 +0400

    Update involved.php

--- views/default/panels/involved.php
+++ views/default/panels/involved.php
@@ -1,12 +1,12 @@
 <div class="box-help box">
-	<h1><i class="fa fa-lightbulb-o"></i> GET INVOLVED</h1>
+	<h1><i class="fa fa-lightbulb-o"></i>GET INVOLVED/REJOIGNEZ NOUS</h1>
 	<section>
-		To Build 
-		<br/> Communicate
-		<br/> Design
-		<br/> Finance
-		<br/> Translate
-		<br/> Innovate
+		<br/>To Build the Communecter platform/Pour faire grandir Communecter
+		<br/> to Communicate and promote/pour communiquer et faire connaître
+		<br/> to design our best interface/ pour concevoir les meilleures interfaces
+		<br/> to Finance/pour apporter un soutien financier
+		<br/> to Help us translate into all languages/pour nous aider à traduire dans toutes les langues
+		<br/> to Innovate/ pour innonver tout simplement
 	</section>
 	<a href="#" class="homestead nextBtns pull-right" onclick="showPanel('box-login')"><?php echo (isset($actionTitle)) ? $actionTitle : "CONNECT" ?></a>
 </div>
