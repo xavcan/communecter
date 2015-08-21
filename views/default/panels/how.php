4b6b63d173895c71adab514d9d02fa31bd2b041f
xavier canal <xaviercanal66@gmail.com>
2015-08-21 14:26:31 +0400

    Update how.php

--- views/default/panels/how.php
+++ views/default/panels/how.php
@@ -1,14 +1,14 @@
 <div class="box-how box">
-	<h1><i class="fa fa-laptop"></i> HOW</h1>
+	<h1><i class="fa fa-laptop"></i> HOW/COMMENT</h1>
 	<section>
-		People, Organizations, Cities and Projects
-		<br/> By The numbers
-		<br/> Computers and more people 
-		<br/> Make a good mix 
-		<br/> Build Great Things
-		<br/> Societal Innovation
-		<br/> Collective Imagination
-		<br/> Open source 
+		People, Organizations, Cities and Projects/Des membres, des associations, des projets et des villes
+		<br/> By The numbers/qui ajoutés les uns aux autres
+		<br/> Computers and more people/avec leurs ordinateurs et leur réseau
+		<br/> Make a good mix/créent,ensemble
+		<br/> Build Great Things/de grands projets
+		<br/> Societal Innovation/de l'innovation sociale
+		<br/> Collective Imagination/collective
+		<br/> Open source/ et open source
 	</section>
 	<a href="#" onclick="showPanel('box-when','bgyellow')" class="homestead nextBtns pull-right">WHEN <i class="fa fa-arrow-circle-o-right"></i></a>
 </div>
