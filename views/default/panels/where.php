351ca510b27049b161be08c3ca8c65c8d7cb6d5d
xavier canal <xaviercanal66@gmail.com>
2015-08-21 14:12:56 +0400

    Update where.php

--- views/default/panels/where.php
+++ views/default/panels/where.php
@@ -1,13 +1,13 @@
 <div class="box-where box">
-	<h1><i class="fa fa-map-marker"></i> WHERE</h1>
+	<h1><i class="fa fa-map-marker"></i> WHERE/N'IMPORTE OU</h1>
 	<section>
-		Every where there are people 
-		<br/> With ideas
-		<br/> Motivation to change
-		<br/> And anyone can start 
-		<br/> Anywhere 
-		<br/> Anytime
-		<br/> Remember you are never alone 
+		Every where there are people/où il y a des hommes (et des femmes)
+		<br/> With ideas/avec des idées
+		<br/> Motivation to change/motivés pour changer le monde
+		<br/> And anyone can start/tout le monde peut commencer 
+		<br/> Anywhere/n'importe où 
+		<br/> Anytime/n'importe quand
+		<br/> Remember you are never alone/ et se souvenir que nous ne sommes jamais seuls
 	</section>
 
 	<a href="#" onclick="showPanel('box-help')" class="homestead nextBtns pull-right">GET INVOLVED <i class="fa fa-arrow-circle-o-right"></i></a>
