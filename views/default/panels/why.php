2027f6d285679d53bb33d26728787037f65349a8
xavier canal <xaviercanal66@gmail.com>
2015-08-21 14:38:40 +0400

    Update why.php

--- views/default/panels/why.php
+++ views/default/panels/why.php
@@ -1,19 +1,19 @@
 <div class="box-why box">
 	<h1><i class="fa fa-heart"></i> WHY</h1>
 	<section class="">
-		Because We Love you
-		<br/> Because Nothings Happens just like that
-		<br/> Create the future you want to live in
-		<br/> Because of the state of things
-		<br/> The Passed is a lesson
-		<br/> A collaborative experiment
-		<br/> To Give a voice to the silent 99%
-		<br/> Sharing not owning
-		<br/> Copy Freely and make it better
-		<br/> Cooperate to Distribute Massively 
-		<br/> Get Together not hide alone
-		<br/> Discover and not Ignore
-		<br/> 
+		Because We Love you/Parce qu'on s'aime
+		<br/> Because Nothings Happens just like that/Parce que rien ne se crée sans raison
+		<br/> Create the future you want to live in/Créez le monde tel que vous le rêvez
+		<br/> Because of the state of things/Parce qu'en tout état de cause
+		<br/> The Passed is a lesson/le passé nous montre le chemin.
+		<br/> A collaborative experiment/Ceci est une expérience collaborative
+		<br/> To Give a voice to the silent 99%/ Donnez une voix à la mjorité silencieuse
+		<br/> Sharing not owning/Partagez, ne possédez pas
+		<br/> Copy Freely and make it better/ Copiez et améliorez sans modération
+		<br/> Cooperate to Distribute Massively /Coopérez et distribuez massivement
+		<br/> Get Together not hide alone/Regroupez vous, ne restez pas seuls
+		<br/> Discover and not Ignore/ Découvrez au lieu d'ignorer
+		<br/> This is why/ C'est comme ça qu'on voit les choses.
 	</section>
 	<a href="#" onclick="showPanel('box-4who','bgblue')" class="homestead nextBtns pull-right">WHO <i class="fa fa-arrow-circle-o-right"></i></a>
 </div>
