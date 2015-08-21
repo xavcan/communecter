467771213bc9cd3468da9589fd0838dc83ec9ecc
xavier canal <xaviercanal66@gmail.com>
2015-08-21 14:48:39 +0400

    Update projects.php

--- views/default/panels/projects.php
+++ views/default/panels/projects.php
@@ -1,8 +1,7 @@
 <div class="box-projects box">
-	<h1><i class="fa fa-lightbulb-o"></i> PROJECTS</h1>
+	<h1><i class="fa fa-lightbulb-o"></i> PROJECTS/PROJETS</h1>
 	<section>
-		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		Il se passe toujours quelque chose... C'est bon à savoir (et à partager) et c'est ici.
 		<u><a class="text-white" href="#" onclick="showAjaxPanel( baseUrl+'/'+moduleId+'/project/projectsv?isNotSV=1', 'ADD A PROJECT','lightbulb-o' )"> Add a Local Project</a> </u>
 		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.
 	</section>
