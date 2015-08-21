6b34e379ef5c79908ceab09e1443304334918162
xavier canal <xaviercanal66@gmail.com>
2015-08-21 14:47:03 +0400

    Update when.php

--- views/default/panels/when.php
+++ views/default/panels/when.php
@@ -1,11 +1,11 @@
 <div class="box-when box">
-	<h1><i class="fa fa-calendar"></i> WHEN</h1>
+	<h1><i class="fa fa-calendar"></i> WHEN/QUAND</h1>
 	
 	<section>
-		If it's not now
-		<br/> it's never
-		<br/> First Beta opening in september 2015
-		<br/> Crowd Funding launches in August 2015
+		If it's not now/C'est maintenant
+		<br/> it's never/Ou jamais/
+		<br/> First Beta opening in september 2015/LA Version Béta en septembre 2015
+		<br/> Crowd Funding launches in August 2015/Le Cowdfounding en Aôut 2015
 	</section>
 	<a href="#" onclick="showPanel('box-where','bgblue')" class="homestead nextBtns pull-right">WHERE <i class="fa fa-arrow-circle-o-right"></i></a>
 </div>
