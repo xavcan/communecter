6a85e57df3ea692f855206703ab7fd763194e319
xavier canal <xaviercanal66@gmail.com>
2015-08-21 14:44:16 +0400

    Update what.php

--- views/default/panels/what.php
+++ views/default/panels/what.php
@@ -1,15 +1,15 @@
 <div class="box-whatisit box">
 	
-	<h1><i class="fa fa-share-alt"></i> WHAT</h1>
+	<h1><i class="fa fa-share-alt"></i> WHAT/QUOI</h1>
 	<section>
-		A new way to live in society
-		<br/> Together to make it better
-		<br/> It's a societal network
-		<br/> Connected to your city 
-		<br/> Building for the commons
-		<br/> Thinking Collectively
-		<br/> Collaborative Economy
-		<br/> Communities drive societies 
+		A new way to live in society/Une nouvelle façon de vivre en société
+		<br/> Together to make it better/pour vivre mieux ensemble
+		<br/> It's a societal network/ Un réseau social
+		<br/> Connected to your city / Connecté à la cité
+		<br/> Building for the commons/ Construit par et pour les communs
+		<br/> Thinking Collectively/ Pensée collective
+		<br/> Collaborative Economy/Economie collaborative
+		<br/> Communities drive societies/ Quand la communauté est aux commandes 
 		<br/> 
 	</section>
 	<hl/>
