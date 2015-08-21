e04948411c47f89b7aa749d318ce65ed3e7e9cd4
xavier canal <xaviercanal66@gmail.com>
2015-08-21 14:19:33 +0400

    Update ph.php

--- views/default/panels/ph.php
+++ views/default/panels/ph.php
@@ -2,12 +2,12 @@
 	<h1><i class="fa fa-cubes"></i> PIXEL HUMAIN </h1>
 	<section>
 		Un collectif magnifique
-		<br/>Innovation au service des biens communs
+		<br/>qui innove au service des biens communs
 		<br/><u><a class="text-white" href="#" onclick="toastr.error('TODO : ajax form load form')"><i class="fa fa-mail"></i> Contact Us </a> </u>
-		<br/>Respectant un CODE SOCIAL ET LOGICIEL ouvert
-		<br/>Open Source, Semantic et Interopérable
-		<br/>PIxel Humain was not yet crowdfunded but has many fans all around the world. 
-		<br/>It is developed by open R & D with contributions from great minds across the globe.
+		<br/>et qui respecte un CODE SOCIAL ET LOGICIEL ouvert.
+		<br/>C'est à dire Open Source, Semantique et Interopérable
+		<br/>Le PIxel Humain n'a pas encore lançé d'opération de crowdfouding (bientôt !)mais nous avons déjà beaucoup d'amis un peu partout sur la planète. 
+		<br/>notre R&D est libre !elle est le fruit de l'imagination d'esprits "éclairés" et tout aussi libres.
 	</section>
 	<a href="#" class="homestead nextBtns pull-right" onclick="showPanel('box-communecter')">COMMUNECTER</a>
 </div>
